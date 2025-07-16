c57891a3c92764cb44e7e82d848d5099ceda20f5
Vijayaraj <Vijayaraj.Anbalagan@gmail.com>
2025-07-16 20:14:04 +0530

    Fixed Typing Indicator

--- src/components/Chatbot.tsx
+++ src/components/Chatbot.tsx
@@ -151,11 +151,13 @@ const quickQuestions = [
   };
 
   const TypingIndicator = () => (
-    <div className="flex items-center space-x-2 p-3 bg-gray-50 rounded-lg max-w-[80%]">
-      <div className="flex items-center space-x-1">
-        <div className="w-2 h-2 bg-orange-400 rounded-full animate-bounce"></div>
-        <div className="w-2 h-2 bg-orange-400 rounded-full animate-bounce" style={{ animationDelay: '0.1s' }}></div>
-        <div className="w-2 h-2 bg-orange-400 rounded-full animate-bounce" style={{ animationDelay: '0.2s' }}></div>
+    <div className="flex justify-start">
+      <div className="bg-gray-100 rounded-lg rounded-bl-none p-3 w-fit">
+        <div className="flex items-center space-x-1">
+          <div className="w-2 h-2 bg-orange-400 rounded-full animate-bounce"></div>
+          <div className="w-2 h-2 bg-orange-400 rounded-full animate-bounce" style={{ animationDelay: '0.1s' }}></div>
+          <div className="w-2 h-2 bg-orange-400 rounded-full animate-bounce" style={{ animationDelay: '0.2s' }}></div>
+        </div>
       </div>
     </div>
   );
